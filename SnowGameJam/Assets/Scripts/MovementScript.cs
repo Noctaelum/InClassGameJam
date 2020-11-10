8f92ff75bd1be8c69fe9e22bd49e17cb9a7f600a
Leo <49573575+Noctaelum@users.noreply.github.com>
2020-11-10 14:52:05 +0000

    MovementAndSpeedBoost

---
+++ SnowGameJam/Assets/Scripts/MovementScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementScript : MonoBehaviour
+{
+
+    public GameObject Character;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*if(canControl == true) {
+            float h = Input.GetAxis("Horizontal") * Speed;
+
+            Character.transform.Translate(h * Time.deltaTime, 0, 0);
+		}*/
+
+		if (Input.GetKey("d")) {
+            Character.transform.Translate(Time.deltaTime * -5, 0, 0);
+        }
+
+        if (Input.GetKey("a")) {
+            Character.transform.Translate(Time.deltaTime * 5, 0, 0);
+        }
+    }
+}
---
+++ SnowGameJam/Assets/Scripts/SpeedBoost.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpeedBoost : MonoBehaviour
+{
+
+    public float slideBoost = 5.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter () {
+        if(other.gameObject.tag == "Player"){
+
+		}
+	}
+}
